be7cd0f77f87329b4828194eaea995156bd30f05
Sebastian Bachmann <me@free-minds.net>
2017-11-15 18:49:51 +0100

    change paths to match debian

--- build/do_configure.sh
+++ build/do_configure.sh
@@ -2,7 +2,7 @@
 cmake \
     -DCMAKE_CXX_COMPILER:FILEPATH=mpic++ \
     -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
-    -DHDF5_LIBRARIES:PATH=/usr2/elan/installs2/lib/libhdf5.a\
-    -DHDF5_INCLUDE_DIRS:PATH=/usr2/elan/installs2/include \
-    -DCMAKE_CXX_FLAGS:STRING="-I/usr2/elan/parosol -L/usr/lib64 -lz"\
+    -DHDF5_LIBRARIES:PATH=/usr/lib/x86_64-linux-gnu/hdf5/serial/libhdf5.so \
+    -DHDF5_INCLUDE_DIRS:PATH=/usr/include/hdf5/serial \
+    -DCMAKE_CXX_FLAGS:STRING="-lz" \
     ../src
