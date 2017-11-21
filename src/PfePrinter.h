b07ebccfe53016e6f8df7ad495f403fd6099adbd
Sebastian Bachmann <me@free-minds.net>
2017-11-21 10:07:08 +0100

    a little bit of clean up

--- src/PfePrinter.h
+++ src/PfePrinter.h
@@ -31,183 +31,183 @@
 
 /*! PfePrinter print the results in the same format as ParFE. In this way,
  *  ParOSol can be used as a fast mesher for ParFE. 
-*/
+ */
 
 
 template <class T>
 class PfePrinter {
-	public:
-		PfePrinter(std::string filename, OctreeGrid<T> &grid):_MyPID(grid.GetPID()), _Size(grid.GetNrCPU()),_filename(filename), _grid(grid)
-		{
-		  Writer = new HDF5_GWriter(filename, MPI_COMM_WORLD);
-		}
-		int _MyPID;
-		int _Size;
-		~PfePrinter()
-		{
-		  Writer->Close();
-		  delete Writer;
-		}
-		
-		void OctKey_to_Coord(long key, int &x, int &y, int &z)
-		{
-			x = y = z = 0;
-			for(int i = 0; i < 16; i++) {
-				x += (key & 1) << i;
-				key = key >> 1;
-				y += (key & 1) << i;
-				key = key >> 1;
-				z += (key & 1) << i;
-				key = key >> 1;
-			}
-		}
+    public:
+        PfePrinter(std::string filename, OctreeGrid<T> &grid):_MyPID(grid.GetPID()), _Size(grid.GetNrCPU()),_filename(filename), _grid(grid)
+    {
+        Writer = new HDF5_GWriter(filename, MPI_COMM_WORLD);
+    }
+        int _MyPID;
+        int _Size;
+        ~PfePrinter()
+        {
+            Writer->Close();
+            delete Writer;
+        }
+
+        void OctKey_to_Coord(long key, int &x, int &y, int &z)
+        {
+            x = y = z = 0;
+            for(int i = 0; i < 16; i++) {
+                x += (key & 1) << i;
+                key = key >> 1;
+                y += (key & 1) << i;
+                key = key >> 1;
+                z += (key & 1) << i;
+                key = key >> 1;
+            }
+        }
 
 
-		void PrintGrid() {
-			PrintCoord("Coordinates");
-			PrintElems("Elements");
-			PrintMaterialIDs("MaterialIDs");
-			PrintBC();
-		}
-		
-		//! Print all Boundary Conditions in to the fixed node Data Set.
-		void PrintBC() {
-		  Writer->Select("/Boundary conditions");
-		  int my_n_fixed_nodes = _grid.bc.FixedNodes_Ind.size();
-		  int n_fixed_nodes;
-		  MPI_Allreduce(&my_n_fixed_nodes, &n_fixed_nodes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-		  int *sense_fixed = new int[my_n_fixed_nodes];
-		  double *value_fixed = new double[my_n_fixed_nodes];
-		  int *node_numbers_fixed = new int[my_n_fixed_nodes];
-		  int myNFixedNodes = 0;
-		  double bc_val, abs_bc_val;
-		  
-		  t_octree_key offset = _grid.GetNodeOffset();
-		  for(int i =0;i<my_n_fixed_nodes;i++) {
-		  	bc_val = _grid.bc.FixedNodes[i];
-		  	abs_bc_val = fabs(bc_val);
-		  	if (abs_bc_val > 1e-14)	{
-		  		node_numbers_fixed[myNFixedNodes] = _grid.bc.FixedNodes_Ind[i]/3 +offset+1;
-		    	sense_fixed[myNFixedNodes] = _grid.bc.FixedNodes_Ind[i]%3+1;
-		    	value_fixed[myNFixedNodes] = bc_val;
-		    	myNFixedNodes++;
-		  	}
-		    // TODO: needed?
-		    /*
-		    if (value[i] == 0)
-		      value[i] = 1e-16;
-		    */
-		  }
+        void PrintGrid() {
+            PrintCoord("Coordinates");
+            PrintElems("Elements");
+            PrintMaterialIDs("Material IDs");
+            PrintBC();
+        }
 
-		  int my_fixed_nodes_offset, N_actual_fixed_nodes;
-		  MPI_Scan(&myNFixedNodes, &my_fixed_nodes_offset,1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-		  MPI_Allreduce(&myNFixedNodes, &N_actual_fixed_nodes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-		  my_fixed_nodes_offset -= myNFixedNodes; //include it selfs
+        //! Print all Boundary Conditions in to the fixed node Data Set.
+        void PrintBC() {
+            Writer->Select("/Boundary conditions");
+            int my_n_fixed_nodes = _grid.bc.FixedNodes_Ind.size();
+            int n_fixed_nodes;
+            MPI_Allreduce(&my_n_fixed_nodes, &n_fixed_nodes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+            int *sense_fixed = new int[my_n_fixed_nodes];
+            double *value_fixed = new double[my_n_fixed_nodes];
+            int *node_numbers_fixed = new int[my_n_fixed_nodes];
+            int myNFixedNodes = 0;
+            double bc_val, abs_bc_val;
 
-		  Writer->Write("Fixed nodes size", (int)N_actual_fixed_nodes);
-		  if(N_actual_fixed_nodes > 0)	{
-		  	  Writer->Write("Fixed nodes", "Node number", node_numbers_fixed,"Sense", sense_fixed, "Value", value_fixed, N_actual_fixed_nodes, myNFixedNodes, 1,1,1, my_fixed_nodes_offset );
-		  }
-		  Writer->Write("Loaded nodes size", (int)0);
-		  Writer->Write("Restrained nodes size", (int)0);
+            t_octree_key offset = _grid.GetNodeOffset();
+            for(int i =0;i<my_n_fixed_nodes;i++) {
+                bc_val = _grid.bc.FixedNodes[i];
+                abs_bc_val = fabs(bc_val);
+                if (abs_bc_val > 1e-14)	{
+                    node_numbers_fixed[myNFixedNodes] = _grid.bc.FixedNodes_Ind[i]/3 +offset+1;
+                    sense_fixed[myNFixedNodes] = _grid.bc.FixedNodes_Ind[i]%3+1;
+                    value_fixed[myNFixedNodes] = bc_val;
+                    myNFixedNodes++;
+                }
+                // TODO: needed?
+                /*
+                   if (value[i] == 0)
+                   value[i] = 1e-16;
+                   */
+            }
 
-		  delete[] sense_fixed;
-		  delete[] value_fixed;
-		  delete[] node_numbers_fixed;
-		}
+            int my_fixed_nodes_offset, N_actual_fixed_nodes;
+            MPI_Scan(&myNFixedNodes, &my_fixed_nodes_offset,1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+            MPI_Allreduce(&myNFixedNodes, &N_actual_fixed_nodes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+            my_fixed_nodes_offset -= myNFixedNodes; //include it selfs
 
-		void PrintMaterialIDs(std::string dset) {
-		    Writer->Select("/Mesh");
+            Writer->Write("Fixed nodes size", (int)N_actual_fixed_nodes);
+            if(N_actual_fixed_nodes > 0)	{
+                Writer->Write("Fixed nodes", "Node number", node_numbers_fixed,"Sense", sense_fixed, "Value", value_fixed, N_actual_fixed_nodes, myNFixedNodes, 1,1,1, my_fixed_nodes_offset );
+            }
+            Writer->Write("Loaded nodes size", (int)0);
+            Writer->Write("Restrained nodes size", (int)0);
+
+            delete[] sense_fixed;
+            delete[] value_fixed;
+            delete[] node_numbers_fixed;
+        }
+
+        void PrintMaterialIDs(std::string dset) {
+            Writer->Select("/Mesh");
             Eigen::VectorXi MatIDs(_grid.GetNrElem());
-			t_index i =0;
-			for(_grid.initIterateOverElements(); _grid.TestIterateOverElements(); _grid.IncIterateOverElements())	{
-				MatIDs[i]=_grid.invEmodMap[_grid.GetElementWeight()];
-				i++;
-			}
+            t_index i =0;
+            for(_grid.initIterateOverElements(); _grid.TestIterateOverElements(); _grid.IncIterateOverElements())	{
+                MatIDs[i]=_grid.invEmodMap[_grid.GetElementWeight()];
+                i++;
+            }
             MPI_Barrier(MPI_COMM_WORLD);
-            Writer->Write("Material IDs", MatIDs.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), 1, _grid.GetElemOffset());
-		}
-		
-		  
-		void PrintCoord(std::string dset) {
-		  Writer->Select("/Mesh");
-		  
-		  
-			t_octree_key k;
-			std::vector<OctreeNode> &grid = _grid.GetOctGrid();
-			std::vector<OctreeNode>::iterator iter; 
-			int x =0,y=0,z=0;
-			double res[3];
-			_grid.GetRes(res);
+            Writer->Write(dset, MatIDs.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), 1, _grid.GetElemOffset());
+        }
+
+
+        void PrintCoord(std::string dset) {
+            Writer->Select("/Mesh");
+
+
+            t_octree_key k;
+            std::vector<OctreeNode> &grid = _grid.GetOctGrid();
+            std::vector<OctreeNode>::iterator iter; 
+            int x =0,y=0,z=0;
+            double res[3];
+            _grid.GetRes(res);
             Eigen::VectorXd coord(_grid.GetNrPrivateNodes()*3);
-			long i=0;
-			for(iter = grid.begin(); iter != _grid._GridIteratorEnd; ++iter ) {
-				k = iter->key;
-				OctKey_to_Coord(k, x, y, z);
-				coord[i++] = x*res[0];
-				coord[i++] = y*res[1];
-				coord[i++] = z*res[2];
-			}
-			Writer->Write(dset, coord.data(), _grid.GetNrNodesGlobal(),_grid.GetNrPrivateNodes(), 3, _grid.GetNodeOffset());
-		}
+            long i=0;
+            for(iter = grid.begin(); iter != _grid._GridIteratorEnd; ++iter ) {
+                k = iter->key;
+                OctKey_to_Coord(k, x, y, z);
+                coord[i++] = x*res[0];
+                coord[i++] = y*res[1];
+                coord[i++] = z*res[2];
+            }
+            Writer->Write(dset, coord.data(), _grid.GetNrNodesGlobal(),_grid.GetNrPrivateNodes(), 3, _grid.GetNodeOffset());
+        }
 
-		void PrintElems(std::string dset) {
-			Writer->Select("/Mesh");
+        void PrintElems(std::string dset) {
+            Writer->Select("/Mesh");
 
-			//print element to node
-			int *elems = new int[_grid.GetNrElem()*8];
-			
-			t_index local_nodes[8];
-			
-			//Quick an dirty hack:
-			//compute the offset with double
+            //print element to node
+            int *elems = new int[_grid.GetNrElem()*8];
+
+            t_index local_nodes[8];
+
+            //Quick an dirty hack:
+            //compute the offset with double
             Eigen::VectorXd ind(_grid.GetNrDofs());
-			_grid.Recv_import_Ghost(ind);
-			ind.setZero(_grid.GetNrDofs());
-			t_octree_key offset = _grid.GetNodeOffset();
-			for (unsigned int i = 0; i < _grid.GetNrNodes(); i++) {
-				ind[3*i] = i + offset+1;
-			}
-			_grid.Send_import_Ghost(ind);
-			_grid.Wait_import_Ghost();
+            _grid.Recv_import_Ghost(ind);
+            ind.setZero(_grid.GetNrDofs());
+            t_octree_key offset = _grid.GetNodeOffset();
+            for (unsigned int i = 0; i < _grid.GetNrNodes(); i++) {
+                ind[3*i] = i + offset+1;
+            }
+            _grid.Send_import_Ghost(ind);
+            _grid.Wait_import_Ghost();
 
-			t_index e =0;
-			for(_grid.initIterateOverElements(); _grid.TestIterateOverElements(); _grid.IncIterateOverElements()){
-				_grid.SearchIndexes(local_nodes); 
-				for(int i =0; i <8; i++)
-					elems[e*8+i] = (int) ind[3*local_nodes[i]];
-				e++;
-			}
-			Writer->Write(dset, elems, _grid.GetNrElemGlobal(),_grid.GetNrElem(), 8, _grid.GetElemOffset());
-			delete[] elems;
-		}
-		
+            t_index e =0;
+            for(_grid.initIterateOverElements(); _grid.TestIterateOverElements(); _grid.IncIterateOverElements()){
+                _grid.SearchIndexes(local_nodes); 
+                for(int i =0; i <8; i++)
+                    elems[e*8+i] = (int) ind[3*local_nodes[i]];
+                e++;
+            }
+            Writer->Write(dset, elems, _grid.GetNrElemGlobal(),_grid.GetNrElem(), 8, _grid.GetElemOffset());
+            delete[] elems;
+        }
+
         void PrintAll(Eigen::VectorXd &x, Eigen::VectorXd &force, Eigen::VectorXd &res) {
 
-			PrintGrid();
+            PrintGrid();
 
-			PostProcess<OctreeGrid<T> > post(_grid);
+            PostProcess<OctreeGrid<T> > post(_grid);
             Eigen::VectorXd m, s, eff;
             Eigen::VectorXd stresses, strains;
-			post.ComputeStressAndStrain(x,m,s,eff,stresses, strains);
-			MPI_Barrier(MPI_COMM_WORLD);
-			
-			Writer->Select("/Solution");
-			Writer->Write("Nodal displacements", x.data(), _grid.GetNrNodesGlobal(),_grid.GetNrPrivateNodes(), 3, _grid.GetNodeOffset());
-			Writer->Write("VonMises", m.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), 1, _grid.GetElemOffset());
-			Writer->Write("SED", s.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), 1, _grid.GetElemOffset());
-			int NstressEntries = 7;
-			int NstrainEntries = 8;
-			Writer->Write("Element stress", stresses.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), NstressEntries, _grid.GetElemOffset());
-			Writer->Write("Element strain", strains.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), NstrainEntries, _grid.GetElemOffset());
-			Writer->Write("Nodal forces", force.data(), _grid.GetNrNodesGlobal(),_grid.GetNrPrivateNodes(), 3, _grid.GetNodeOffset());
-		}
+            post.ComputeStressAndStrain(x,m,s,eff,stresses, strains);
+            MPI_Barrier(MPI_COMM_WORLD);
 
-		std::string _filename;
+            Writer->Select("/Solution");
+            Writer->Write("Nodal displacements", x.data(), _grid.GetNrNodesGlobal(),_grid.GetNrPrivateNodes(), 3, _grid.GetNodeOffset());
+            Writer->Write("VonMises", m.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), 1, _grid.GetElemOffset());
+            Writer->Write("SED", s.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), 1, _grid.GetElemOffset());
+            int NstressEntries = 7;
+            int NstrainEntries = 8;
+            Writer->Write("Element stress", stresses.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), NstressEntries, _grid.GetElemOffset());
+            Writer->Write("Element strain", strains.data(), _grid.GetNrElemGlobal(),_grid.GetNrElem(), NstrainEntries, _grid.GetElemOffset());
+            Writer->Write("Nodal forces", force.data(), _grid.GetNrNodesGlobal(),_grid.GetNrPrivateNodes(), 3, _grid.GetNodeOffset());
+        }
 
-		OctreeGrid<T> &_grid;
-		
-		HDF5_GWriter *Writer;
+        std::string _filename;
+
+        OctreeGrid<T> &_grid;
+
+        HDF5_GWriter *Writer;
 
 };
 #endif /* PFEPRINTER */
--- src/main.cpp
+++ src/main.cpp
@@ -35,11 +35,8 @@
 #include "Problem.h"
 #include "MlCycle.h"
 
-#include "VTKPrinter.h"
-#include "HDF5Printer.h"
-#include "HDF5ParfePrinter.h"
 #include "PfePrinter.h"
- 
+
 #define EXIT(X) MPI_Finalize(); exit(X);
 
 //measure mflops with a matrix
@@ -53,26 +50,26 @@ double global_poisson_ratio;
 
 int main( int argc, char* argv[])
 {
-	MPI_Init(&argc, &argv);
-	int pid, psize;
-	MPI_Comm_rank (MPI_COMM_WORLD, &pid);
-	int MyPID = pid;
-	MPI_Comm_size (MPI_COMM_WORLD, &psize);
+    MPI_Init(&argc, &argv);
+    int pid, psize;
+    MPI_Comm_rank (MPI_COMM_WORLD, &pid);
+    int MyPID = pid;
+    MPI_Comm_size (MPI_COMM_WORLD, &psize);
 
-	std::string file;
+    std::string file;
     int level = 6, degree = 10;
     double tol = 1e-6;
 
     std::string usage = std::string("usage: ") + argv[0]
-                      + std::string(" [--level arg (6)]")
-                      + std::string(" [--tol arg (1e-6)]")
-                      + std::string(" filename\n"); 
-	
+        + std::string(" [--level arg (6)]")
+        + std::string(" [--tol arg (1e-6)]")
+        + std::string(" filename\n"); 
+
     if (argc < 2) {
-		PCOUT(MyPID, usage)
-		MPI_Finalize();
-		return -1;
-	}
+        PCOUT(MyPID, usage)
+            MPI_Finalize();
+        return -1;
+    }
 
     int i = 1;
     std::string param;
@@ -91,137 +88,134 @@ int main( int argc, char* argv[])
         i++;
     }
 
-	PCOUT(MyPID, "file: " << file << " tolerance: " << tol << " max. num. level: "<< level << std::endl);
+    PCOUT(MyPID, "file: " << file << " tolerance: " << tol << " max. num. level: "<< level << std::endl);
     if (file.compare("") == 0) {
-		PCOUT(MyPID, "No file given\n");
-		MPI_Finalize();
-		return -10;
+        PCOUT(MyPID, "No file given\n");
+        MPI_Finalize();
+        return -10;
     }
 
-	Timer timer(MPI_COMM_WORLD);
-	timer.Start("All");
+    Timer timer(MPI_COMM_WORLD);
+    timer.Start("All");
 
-	t_timing elapsed_time;
+    t_timing elapsed_time;
 
-	CPULayout layout;
+    CPULayout layout;
 
     //Read in the Problem
-	timer.Start("Read");
-	HDF5Image ir(file, layout);
-	typedef OctreeGrid<OctreeKey_Lookup> t_Ogrid;
-	t_Ogrid grid(ir);
-	timer.Stop("Read");
-	elapsed_time = timer.ElapsedTime("Read");
-	PCOUT(MyPID, "Time for Read in: "  << COUTTIME(elapsed_time) << "s\n");
+    timer.Start("Read");
+    HDF5Image ir(file, layout);
+    typedef OctreeGrid<OctreeKey_Lookup> t_Ogrid;
+    t_Ogrid grid(ir);
+    timer.Stop("Read");
+    elapsed_time = timer.ElapsedTime("Read");
+    PCOUT(MyPID, "Time for Read in: "  << COUTTIME(elapsed_time) << "s\n");
 
-	PCOUT(MyPID, "Nr. of global elements: " << grid.GetNrElemGlobal() << " global nodes: " << grid.GetNrNodesGlobal() << "\n");
+    PCOUT(MyPID, "Nr. of global elements: " << grid.GetNrElemGlobal() << " global nodes: " << grid.GetNrNodesGlobal() << "\n");
 
     //Generate the BC
-	timer.Start("BC");
-	grid.GenerateBC();
-	timer.Stop("BC");
-	elapsed_time = timer.ElapsedTime("BC");
-	PCOUT(MyPID, "Time for Construction of Boundary Condition: " << COUTTIME(elapsed_time) << "s\n");
+    timer.Start("BC");
+    grid.GenerateBC();
+    timer.Stop("BC");
+    elapsed_time = timer.ElapsedTime("BC");
+    PCOUT(MyPID, "Time for Construction of Boundary Condition: " << COUTTIME(elapsed_time) << "s\n");
 
-	PCOUT(MyPID, grid);
+    PCOUT(MyPID, grid);
 
     //Construct the matrix
-	timer.Start("Mat");
-	GenericMatrix<t_Ogrid> matr(grid);
-	timer.Stop("Mat");
-	elapsed_time = timer.ElapsedTime("Mat");
-	PCOUT(MyPID, "Time for Construction of Matrix: " << COUTTIME(elapsed_time) << "s\n");
-	PCOUT(MyPID, matr);
+    timer.Start("Mat");
+    GenericMatrix<t_Ogrid> matr(grid);
+    timer.Stop("Mat");
+    elapsed_time = timer.ElapsedTime("Mat");
+    PCOUT(MyPID, "Time for Construction of Matrix: " << COUTTIME(elapsed_time) << "s\n");
+    PCOUT(MyPID, matr);
 
-	std::cout << std::setprecision (13); 
-	MPI_Barrier(MPI_COMM_WORLD);
+    std::cout << std::setprecision (13); 
+    MPI_Barrier(MPI_COMM_WORLD);
 
     //Construct the preconditioner
-	timer.Start("Prec");
-	MlCycle<OctreeKey_Lookup>  prec(grid, matr, degree, level-1, 0, 16,0);
-	PCOUT(MyPID, "Prec done\n");
-	timer.Stop("Prec");
-	elapsed_time = timer.ElapsedTime("Prec");
-	PCOUT(MyPID, "Time for Building Preconditioner: " << COUTTIME(elapsed_time) << "s\n");
+    timer.Start("Prec");
+    MlCycle<OctreeKey_Lookup>  prec(grid, matr, degree, level-1, 0, 16,0);
+    PCOUT(MyPID, "Prec done\n");
+    timer.Stop("Prec");
+    elapsed_time = timer.ElapsedTime("Prec");
+    PCOUT(MyPID, "Time for Building Preconditioner: " << COUTTIME(elapsed_time) << "s\n");
 
     //Setting up the problem
-	timer.Start("Setup");
+    timer.Start("Setup");
     int maxiter = 1000;
     int outputfreq = 4;
-	PCGSolver solver(matr, prec, tol, maxiter, true, outputfreq);
-	Problem<t_Ogrid> problem(matr);
-	if (problem.Impose())
-		std::cout << "Error in impose\n";
-	problem.SetSolver(solver);
-	timer.Stop("Setup");
-	elapsed_time =  timer.ElapsedTime("Setup");
-	PCOUT(MyPID, "Time for Setting up the Problem: " << COUTTIME(elapsed_time) << "s\n");
+    PCGSolver solver(matr, prec, tol, maxiter, true, outputfreq);
+    Problem<t_Ogrid> problem(matr);
+    if (problem.Impose())
+        std::cout << "Error in impose\n";
+    problem.SetSolver(solver);
+    timer.Stop("Setup");
+    elapsed_time =  timer.ElapsedTime("Setup");
+    PCOUT(MyPID, "Time for Setting up the Problem: " << COUTTIME(elapsed_time) << "s\n");
 
     //solving the system
-	double resid = problem.Res();
-	PCOUT(MyPID, "Residuum before solving: " << resid << std::endl);
-	timer.Start("Solve");
-	problem.Solve();
-	resid = problem.Res();
-	PCOUT(MyPID, "Residuum after solving: " << resid << std::endl);
-	timer.Stop("Solve");
-	t_timing time = timer.ElapsedTime("Solve");
-	PCOUT(MyPID, "Solving time: " << COUTTIME(time) << "s\n");
+    double resid = problem.Res();
+    PCOUT(MyPID, "Residuum before solving: " << resid << std::endl);
+    timer.Start("Solve");
+    problem.Solve();
+    resid = problem.Res();
+    PCOUT(MyPID, "Residuum after solving: " << resid << std::endl);
+    timer.Stop("Solve");
+    t_timing time = timer.ElapsedTime("Solve");
+    PCOUT(MyPID, "Solving time: " << COUTTIME(time) << "s\n");
 
     //print out the solution
     print(matr, problem, file, MyPID);
 
-	timer.Stop("All");
-	elapsed_time = timer.ElapsedTime("All");
+    timer.Stop("All");
+    elapsed_time = timer.ElapsedTime("All");
 
-	PCOUT(MyPID, "Overall time: " << COUTTIME(elapsed_time) << "s\n");
+    PCOUT(MyPID, "Overall time: " << COUTTIME(elapsed_time) << "s\n");
 
-	//mflops(matr, MyPID);
+    //mflops(matr, MyPID);
 
-	MPI_Finalize();
-	return 0;
+    MPI_Finalize();
+    return 0;
 }
 
 template <class T>
 void print(GenericMatrix<T> &matr, Problem<T> &problem, std::string file, int MyPID) {
-	Timer timer(MPI_COMM_WORLD);
-	//VTKPrinter<OctreeKey_Lookup> print("out", matr.GetGrid());
-	//HDF5Printer<OctreeKey_Lookup> print(file, matr.GetGrid());
-	//HDF5ParfePrinter<OctreeKey_Lookup> print2(file, matr.GetGrid());
-	PfePrinter<OctreeKey_Lookup> print3(file, matr.GetGrid());
-	matr.PrintTimings();
-	timer.Start("Write");
+    Timer timer(MPI_COMM_WORLD);
+
+    PfePrinter<OctreeKey_Lookup> print3(file, matr.GetGrid());
+    matr.PrintTimings();
+
+    timer.Start("Write");
     int dofs = matr.GetGrid().GetNrDofs();
     Eigen::VectorXd force(dofs);
     matr.Apply_NoResetBoundaries(problem.GetSol(), force);
-	//print.PrintAll(problem.GetSol(), force, problem.GetRes());
-	//print2.PrintAll(problem.GetSol(), force, problem.GetRes());
-	print3.PrintAll(problem.GetSol(), force, problem.GetRes());
-	timer.Stop("Write");
-	t_timing time = timer.ElapsedTime("Write");
-	PCOUT(MyPID, "Outputtime: " << COUTTIME(time) << "s\n");
+    print3.PrintAll(problem.GetSol(), force, problem.GetRes());
+    timer.Stop("Write");
+    t_timing time = timer.ElapsedTime("Write");
+    PCOUT(MyPID, "Outputtime: " << COUTTIME(time) << "s\n");
 }
 
 template <class T>
 void mflops(GenericMatrix<T> &matr, int MyPID) {
-	Timer timer(MPI_COMM_WORLD);
-	int dofs = matr.GetGrid().GetNrDofs(); 
+    Timer timer(MPI_COMM_WORLD);
+    int dofs = matr.GetGrid().GetNrDofs(); 
     Eigen::VectorXd a,b;
-	a.setRandom(dofs);
-	b.setZero(dofs);
-	t_octree_key globalelem = matr.GetGrid().GetNrElemGlobal();
-	int size = matr.GetGrid().GetNrCPU();
-	int loops = 16e6/globalelem*size+1;
-	double flop = globalelem*(24*(24+24)+48)*loops; //+48 the factor
+    a.setRandom(dofs);
+    b.setZero(dofs);
+    t_octree_key globalelem = matr.GetGrid().GetNrElemGlobal();
+    int size = matr.GetGrid().GetNrCPU();
+    int loops = 16e6/globalelem*size+1;
+    double flop = globalelem*(24*(24+24)+48)*loops; //+48 the factor
 
-	PCOUT(MyPID, "Meassure with " << loops << "loops" << std::endl);
-	MPI_Barrier(MPI_COMM_WORLD);
-	timer.Start("gflop");
-	for(int i = 0; i <loops; i++)
-		matr.Apply_NoResetBoundaries(a, b);
-	timer.Stop("gflop");
-	t_timing ela = timer.ElapsedTime("gflop");
-	double elaps = ela.max; 
-	PCOUT(MyPID, "time needed: " << elaps << std::endl)
-	PCOUT(MyPID, "MFlop/s: " << flop/elaps/1e6<< std::endl)
+    PCOUT(MyPID, "Meassure with " << loops << "loops" << std::endl);
+    MPI_Barrier(MPI_COMM_WORLD);
+    timer.Start("gflop");
+    for(int i = 0; i <loops; i++)
+        matr.Apply_NoResetBoundaries(a, b);
+    timer.Stop("gflop");
+    t_timing ela = timer.ElapsedTime("gflop");
+    double elaps = ela.max; 
+    PCOUT(MyPID, "time needed: " << elaps << std::endl)
+        PCOUT(MyPID, "MFlop/s: " << flop/elaps/1e6<< std::endl)
 }
